2fd150d63988b69213b8e4c77baafc41354c8781
Avinash Singh <avinash.dhillon@gmail.com>
2019-06-02 13:14:52 -0500

    Created initial HTML structure and initial css styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//create the character objects
+
+let darthVader = {
+    healthPoints: 180,
+    attackPower: 6,
+    counterAttackPower: 25,
+};
+
+let hansSolo = {
+    healthPoints: 120,
+    attackPower: 6,
+    counterAttackPower: 10,
+};
+
+let lukeSkywalker = {
+    healthPoints = 100,
+    attackPower: 6,
+    counterAttackPower: 5,
+};
+
+let yoda = {
+    healthPoints = 120,
+    attackPower: 6,
+    counterAttackPower: 20,
+};
+
