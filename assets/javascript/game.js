62119eff40b0b02a382b75abb2c70f3be60fa808
Avinash Singh <avinash.dhillon@gmail.com>
2019-06-02 17:00:22 -0500

    Finished layout of combatants when clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
-//create the character objects
+//Global variables
+let yourPlayer;
 
+
+//create the character objects
 let darthVader = {
     healthPoints: 180,
     attackPower: 6,
@@ -13,14 +16,49 @@ let hansSolo = {
 };
 
 let lukeSkywalker = {
-    healthPoints = 100,
+    healthPoints: 100,
     attackPower: 6,
     counterAttackPower: 5,
 };
 
 let yoda = {
-    healthPoints = 120,
+    healthPoints: 120,
     attackPower: 6,
     counterAttackPower: 20,
 };
 
+$("#charIcons").click(function (event){
+    //let idVal = $(EventTarget).attr("id");
+    selectYourChar($(event.target).attr("id"));
+});
+
+$("#enemyCharIcons").click(function (event){
+    //let idVal = $(EventTarget).attr("id");
+    selectEnemy($(event.target).attr("id"));
+})
+
+function selectYourChar (idValue) {
+    //change the border color to green for friendly combatant
+    $("#" + idValue).css ({
+        "border-color": "darkGreen",
+    })
+
+    //move this icon to "Your Character" section
+    let parentEle = $("#yourCharIcon").append($("#" + idValue));
+    $(selectCharMsg).css({
+        "opacity": 0,
+    });
+
+    //debugger;
+    //move the remaining conbatants to the "Enemies Available" section
+    $("#enemyCharIcons").append($("#charIcons").children());
+
+    //Change the border color to red for enemy combatants
+    $("#enemyCharIcons").children().css({
+        "border-color": "red",
+    });
+}
+
+function selectEnemy(idValue){
+    $("#enemyIcon").append($("#" + idValue));
+}
