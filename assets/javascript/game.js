126515619df4459882d0be13e9ff8528f44311ba
Avinash Singh <avinash.dhillon@gmail.com>
2019-06-03 17:49:53 -0500

    Merged Laptop and Desktop code manually

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,60 @@
 //Global variables
 let yourPlayer;
-
+let enemyPlayer;
+let yourAttackPower = 0;
+let defenderHealth = 0;
+let enemyLoaded = false;
 
 //create the character objects
-let darthVader = {
-    healthPoints: 180,
-    attackPower: 6,
-    counterAttackPower: 25,
-};
 
 let hansSolo = {
     healthPoints: 120,
+    baseAttack: 6,
     attackPower: 6,
     counterAttackPower: 10,
 };
 
 let lukeSkywalker = {
     healthPoints: 100,
+    baseAttack: 6,
     attackPower: 6,
     counterAttackPower: 5,
 };
 
 let yoda = {
     healthPoints: 120,
-    attackPower: 6,
+    baseAttack: 7,
+    attackPower: 7,
     counterAttackPower: 20,
 };
 
+let darthVader = {
+    healthPoints: 180,
+    baseAttack: 7,
+    attackPower: 7,
+    counterAttackPower: 25,
+};
+
+//these are the click events
+
 $("#charIcons").click(function (event){
-    //let idVal = $(EventTarget).attr("id");
     selectYourChar($(event.target).attr("id"));
+    //assign this character's object to "Your Character"
+    yourPlayer = assignCharObj($(event.target).attr("id"));
 });
 
 $("#enemyCharIcons").click(function (event){
-    //let idVal = $(EventTarget).attr("id");
     selectEnemy($(event.target).attr("id"));
-})
+    //assign this character's object to "Defender/Enemy Character"
+    enemyPlayer = assignCharObj($(event.target).attr("id"));
+});
+
+$("#butAtt").click(function () {
+
+});
+
+
+//these are the named functions
 
 function selectYourChar (idValue) {
     //change the border color to green for friendly combatant
@@ -49,7 +68,6 @@ function selectYourChar (idValue) {
         "opacity": 0,
     });
 
-    //debugger;
     //move the remaining conbatants to the "Enemies Available" section
     $("#enemyCharIcons").append($("#charIcons").children());
 
@@ -62,3 +80,50 @@ function selectYourChar (idValue) {
 function selectEnemy(idValue){
     $("#enemyIcon").append($("#" + idValue));
 }
+
+//
+function assignCharObj (charName){
+    debugger;
+switch (charName) {
+
+    case "hansSolo":
+        return hansSolo;
+        break;
+    case "lukeSkywalker":
+        return lukeSkywalker;
+        break;
+    case "yoda":
+        return yoda;
+        break;
+    case "darthVader":
+        return darthVader;
+        break;
+    default:
+        return darthVader;
+        break;
+    }
+}
+
+//when player clicks the attack button
+$("#butAtt").click(function (){
+    debugger;
+    if(!enemyLoaded) {
+        return;
+    }
+    //the player attack lowers enemy health points
+    yourEnemy.healthPoints =- yourPlayer.attackPower;
+
+    //the enemy's counter attack lowers your player's HP
+    yourPlayer.healthPoints =- yourEnemy.counterAttackPower;
+    
+    //if your player survives, increase their attack power;
+    if(yourPlayer.healthPoints > 0){
+        yourPlayer.attackPower =+ yourPlayer.baseAttack;
+    }else {
+        //your player has lost the game
+        ////////// add code for loss/////////////
+    }
+
+    //if the enemy player has lost all its HP select another enemy if possible
+    //if()
+});
